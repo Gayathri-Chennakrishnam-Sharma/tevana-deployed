0c504f2ad308cf72d2fd282e7c29cbda692937a1
gayathri-chennakrishnam-au16 <sharmaanaga620@gmail.com>
2021-06-07 05:21:49 +0530

    deployment changes

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ export const bookGarden = async gardenId => {
     // this.stripe = await loadStripe("pk_test_51Iz4jeSJZMM3bl9Qllm7TZzswGDn3ZVgzCRxOH7FAU7wUiuM2ugDNmM4p4M0EDvLPpAbopEd0P8X0LjA1dmnI7eD00qrpw7Ytv");
     // 1) Get checkout session from API
     const session = await axios(
-      `http://127.0.0.1:4000/api/v1/booking/checkout-session/${gardenId}`
+      `/api/v1/booking/checkout-session/${gardenId}`
     );
     console.log(session);
 
