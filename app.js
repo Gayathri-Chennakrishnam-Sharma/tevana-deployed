62af6d3a22045ffd2babfe37a16f3a41b9794e13
divyansh_sharma_au16 <divyanshsharma7765@gmail.com>
2021-06-10 22:39:16 +0530

    cors implemented

--- app.js
+++ app.js
@@ -1,37 +1,116 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-const compression = require('compression');
-const cookieParser = require('cookie-parser');
-const stripe = require('stripe');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+const stripe = require("stripe");
+const cors = require("cors");
+const csp = require("express-csp");
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/globalErrorHndler');
-const gardenRouter = require('./routes/gardenRoute');
-const userRouter = require('./routes/userRoute');
-const bookingRouter = require('./routes/bookingRoute');
-const viewRouter = require('./routes/viewRoute');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/globalErrorHndler");
+const gardenRouter = require("./routes/gardenRoute");
+const userRouter = require("./routes/userRoute");
+const bookingRouter = require("./routes/bookingRoute");
+const viewRouter = require("./routes/viewRoute");
 
 const app = express();
+app.enable("trust proxy");
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(cors());
+
+app.options("*", cors());
+
+//serving static files
+app.use(express.static(path.join(__dirname, "public")));
 
 //global middlewares
 //set security HTTP
+// app.use(helmet());
+
 app.use(helmet());
+csp.extend(app, {
+  policy: {
+    directives: {
+      "default-src": ["self"],
+      "style-src": ["self", "unsafe-inline", "https:"],
+      "font-src": ["self", "https://fonts.gstatic.com"],
+      "script-src": [
+        "self",
+        "unsafe-inline",
+        "data",
+        "blob",
+        "https://js.stripe.com",
+        "https://*.mapbox.com",
+        "https://*.cloudflare.com/",
+        "https://bundle.js:8828",
+        "ws://localhost:56558/",
+      ],
+      "worker-src": [
+        "self",
+        "unsafe-inline",
+        "data:",
+        "blob:",
+        "https://*.stripe.com",
+        "https://*.mapbox.com",
+        "https://*.cloudflare.com/",
+        "https://bundle.js:*",
+        "ws://localhost:*/",
+      ],
+      "frame-src": [
+        "self",
+        "unsafe-inline",
+        "data:",
+        "blob:",
+        "https://*.stripe.com",
+        "https://*.mapbox.com",
+        "https://*.cloudflare.com/",
+        "https://bundle.js:*",
+        "ws://localhost:*/",
+      ],
+      "img-src": [
+        "self",
+        "unsafe-inline",
+        "data:",
+        "blob:",
+        "https://*.stripe.com",
+        "https://*.mapbox.com",
+        "https://*.cloudflare.com/",
+        "https://bundle.js:*",
+        "ws://localhost:*/",
+      ],
+      "connect-src": [
+        "self",
+        "unsafe-inline",
+        "data:",
+        "blob:",
+        // "wss://https://blooming-peak-67620.herokuapp.com/",
+        "https://*.stripe.com",
+        "https://bundle.js:*",
+        "ws://localhost:*/",
+      ],
+    },
+  },
+});
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(compression());
 
 //body parser
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 app.use(cookieParser());
 
@@ -42,25 +121,21 @@ app.use((req, res, next) => {
 });
 
 // eslint-disable-next-line prefer-arrow-callback
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy',
-    "script-src 'self' https://cdnjs.cloudflare.com"
-  );
-  next();
-});
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+// app.use(function (req, res, next) {
+//   res.setHeader(
+//     'Content-Security-Policy',
+//     "script-src 'self' https://cdnjs.cloudflare.com"
+//   );
+//   next();
+// });
 
 //limit request
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too Many requests from this IP ,please try again after an hour',
+  message: "Too Many requests from this IP ,please try again after an hour",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 app.use(express.json());
 
@@ -69,16 +144,14 @@ app.use(mongoSanitize());
 app.use(xss());
 
 app.use(hpp());
-//serving static files
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/', viewRouter);
-app.use('/api/v1/gardens', gardenRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/booking', bookingRouter);
+app.use("/", viewRouter);
+app.use("/api/v1/gardens", gardenRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/booking", bookingRouter);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`can't find ${req.originalUrl} on the server`, 404));
 });
 
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,24 +1,23 @@
-const stripe = Stripe('pk_test_51Iz4jeSJZMM3bl9Qllm7TZzswGDn3ZVgzCRxOH7FAU7wUiuM2ugDNmM4p4M0EDvLPpAbopEd0P8X0LjA1dmnI7eD00qrpw7Ytv');
-import axios from 'axios';
-import { showAlert } from './alerts';
+import axios from "axios";
+import { showAlert } from "./alerts";
 // import {loadStripe} from '@stripe/stripe-js';
 
-
-export const bookGarden = async gardenId => {
+export const bookGarden = async (gardenId) => {
+  const stripe = Stripe(
+    "pk_test_51Iz4jeSJZMM3bl9Qllm7TZzswGDn3ZVgzCRxOH7FAU7wUiuM2ugDNmM4p4M0EDvLPpAbopEd0P8X0LjA1dmnI7eD00qrpw7Ytv"
+  );
   try {
     // this.stripe = await loadStripe("pk_test_51Iz4jeSJZMM3bl9Qllm7TZzswGDn3ZVgzCRxOH7FAU7wUiuM2ugDNmM4p4M0EDvLPpAbopEd0P8X0LjA1dmnI7eD00qrpw7Ytv");
     // 1) Get checkout session from API
-    const session = await axios(
-      `/api/v1/booking/checkout-session/${gardenId}`
-    );
+    const session = await axios(`/api/v1/booking/checkout-session/${gardenId}`);
     console.log(session);
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
+      sessionId: session.data.session.id,
     });
   } catch (err) {
     console.log(err);
-    showAlert('error', err);
+    showAlert("error", err);
   }
 };
